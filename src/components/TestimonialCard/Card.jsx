0f92be66424917903f7ece9f89d1d0f5a48df7f8
roshan <mahatoroshan1999@gmail.com>
2023-06-08 00:04:46 +0530

    fix: unwanted import

--- src/components/TestimonialCard/Card.jsx
+++ src/components/TestimonialCard/Card.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles.scss";
-import useWindowWidth from "../../customHooks/useWindowWidth";
 
 function TestimonialCard({ name, designation, reviewMsg }) {
   return (
